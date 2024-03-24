c172ba565caac76e585f4bf96506212997d56b72
Gyudong Kim <dong3955@gmail.com>
2024-03-24 20:41:13 +0900

    [Chore] Apply Font

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Recipe App',
-      home: RecipePage(),
+      theme: ThemeData(
+        fontFamily: "PatuaOne",
+      ),
+      home: const RecipePage(),
     );
   }
 }
